6f724912a683105b835aa4401b0537f71238f01c
Denis Kibet <kibet1816@gmail.com>
2019-06-27 07:17:34 +0300

    Add docstring to configuratiion function

--- app/request.py
+++ app/request.py
@@ -19,6 +19,9 @@ search_url = None
 
 
 def configure_request(app):
+    """
+    Method to configure requests
+    """
     global api_key,base_url,article_url,topheadline_url,everything_url,search_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config["SOURCE_API_BASE_URL"]
